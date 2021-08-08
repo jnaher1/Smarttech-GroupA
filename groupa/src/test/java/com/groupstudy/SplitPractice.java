aecc7c8590c10f23d972fa626c2f1e9e4f3c619e
jnaher1 <jesmin_soc@yahoo.com>
2021-08-08 10:45:17 -0500

    split method

---
+++ groupa/src/test/java/com/groupstudy/SplitPractice.java
@@ -0,0 +1,19 @@
+package com.groupstudy;
+
+public class SplitPractice {
+
+	public static void main(String[] args) {
+
+ String country = "United States of America";
+  String[] parts = country.split(" ");
+   for( String a:parts) {
+	   System.out.println(a);
+   }
+ 
+ 
+   
+ 
+
+	}
+
+}
